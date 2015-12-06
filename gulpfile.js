438b5ebe7f48915096a900b14be7e08ebd076384
winslow <winslow@winslow-HP-540.(none)>
2015-12-06 20:00:42 +0800

    构建目录

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var jshint=require('gulp-jshint');
+var less = require('gulp-less');
+var rev = require('gulp-rev-append');
+var clean = require('gulp-clean');
+
+// 更新版本号
+gulp.task('revHTML', function() {
+    return gulp.src('./*.html')
+        .pipe(rev())
+        .pipe(gulp.dest('./'));
+});
+// less解析
+gulp.task('build-less',['cleanCSS'], function(){
+    return gulp.src('./src/less/style.less')
+        .pipe(less())
+        .pipe(gulp.dest('./build/css/'));
+});
+
+// 压缩、重命名css
+gulp.task('stylesheets',['build-less','revHTML'], function() {
+    return gulp.src(['./build/css/*.css'])
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./build/css'));
+});
+
+//压缩 js
+gulp.task('minifyjs',['cleanJS','revHTML'], function() {
+    return gulp.src('./src/js/*.js')      //需要操作的文件
+        .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
+        .pipe(uglify())    //压缩
+        .pipe(gulp.dest('./build/js'))  //输出
+});
+
+// 清空css
+gulp.task('cleanCSS', function() {
+  return gulp.src('./build/css/*.css', {read: false})
+    .pipe(clean({force: true}));
+});
+// 清空js
+gulp.task('cleanJS', function() {
+  return gulp.src('./build/js/main.min.js', {read: false})
+    .pipe(clean({force: true}));
+});
+
+//监听
+gulp.task('watch', function() {
+    gulp.watch('./src/js/*.js', ['minifyjs']);
+    gulp.watch('./src/less/*.less', ['stylesheets']);
+    gulp.watch('./src/*.html', ['revHTML']);
+});
+
+gulp.task('default',['watch']);
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+var a = 1;
+var b = 2;
+if(a == b)console.log('yes');
+var t =5;
+alert('s');
+contains('efefef');
