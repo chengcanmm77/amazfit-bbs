35fbcc9df19e63b1e57707fd1ab58934834498a7
sw-wang <wsw.sun@qq.com>
2015-12-08 20:17:17 +0800

    修改

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,14 @@ var jshint=require('gulp-jshint');
 var less = require('gulp-less');
 var rev = require('gulp-rev-append');
 var imagemin = require('gulp-imagemin');
+var contentIncluder = require('gulp-content-includer');
 var clean = require('gulp-clean');
 
-// 更新版本号
-gulp.task('revHTML', function() {
-    return gulp.src('./src/*.html')
-        .pipe(rev())
+gulp.task('html', function() {
+    return gulp.src("./src/*.html")
+        .pipe(contentIncluder({
+            includerReg:/<!\-\-include\s+"([^"]+)"\-\->/g
+        }))
         .pipe(gulp.dest('./build'));
 });
 // 
@@ -21,45 +23,45 @@ gulp.task('img', function(){
         .pipe(imagemin({progressive: true}))
         .pipe(gulp.dest('./build/img'));
 })
-// less解析
+// 
 gulp.task('build-less',['cleanCSS'], function(){
     return gulp.src('./src/less/style.less')
         .pipe(less())
         .pipe(gulp.dest('./build/css/'));
 });
 
-// 压缩、重命名css
-gulp.task('stylesheets',['build-less','revHTML'], function() {
+// 
+gulp.task('stylesheets',['build-less'], function() {
     return gulp.src(['./build/css/*.css'])
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
         .pipe(gulp.dest('./build/css'));
 });
 
-//压缩 js
-gulp.task('minifyjs',['cleanJS','revHTML'], function() {
-    return gulp.src('./src/js/*.js')      //需要操作的文件
-        .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
-        .pipe(uglify())    //压缩
-        .pipe(gulp.dest('./build/js'))  //输出
+// 
+gulp.task('minifyjs',['cleanJS'], function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'))
 });
 
-// 清空css
+// 
 gulp.task('cleanCSS', function() {
   return gulp.src('./build/css/*.css', {read: false})
     .pipe(clean({force: true}));
 });
-// 清空js
+// 
 gulp.task('cleanJS', function() {
   return gulp.src('./build/js/main.min.js', {read: false})
     .pipe(clean({force: true}));
 });
 
-//监听
+//
 gulp.task('watch', function() {
     gulp.watch('./src/js/*.js', ['minifyjs']);
     gulp.watch('./src/less/*.less', ['stylesheets']);
-    gulp.watch('./src/*.html', ['revHTML']);
+    gulp.watch('./src/*.html', ['html']);
 });
 
 gulp.task('default',['watch']);
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
-var a = 1;
-var b = 2;
-if(a == b)console.log('yes');
-var t =5;
-alert()
+$(function(){
+    // 搜索下拉框
+    $('ul.search-type').click(function(){
+        $('.btn-subtype').toggleClass('btn-subtype-rotat');
+        $('.subtype-wrap').slideToggle(200);
+    })
+    // 推荐用户排版
+    $('ul.ul-users').find('li').each(function(index, el) {
+        if( (index+1)%3 == 0 )$(this).css('marginRight','0px');
+    });
+    // 微信
+    $('#wechat').on('mouseenter', function(){
+        $(this).find('img').stop().fadeIn();
+    }).on('mouseleave', function(){
+        $(this).find('img').stop().fadeOut();
+    })
+});
